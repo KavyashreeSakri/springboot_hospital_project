bdfe2ac0226a0fac28f433bd7bb8eeb5b391fd1b
KavyashreeSakri <kavyashree30362@gmail.com>
2023-02-17 15:05:53 +0530

    first commit

---
+++ src/main/java/com/ty/springboot_project/exception/IdNotFoundException.java
@@ -0,0 +1,16 @@
+package com.ty.springboot_project.exception;
+
+public class IdNotFoundException extends RuntimeException {
+
+	private String message;
+
+	public String getMessage() {
+		return message;
+	}
+
+	public IdNotFoundException(String message) {
+		super();
+		this.message = message;
+	}
+
+}
